25a392eeac8af0940aa4c276da5f2f15742400b0
zogeek <91289024+zogeek@users.noreply.github.com>
2026-03-31 13:22:02 +0200

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,7 @@ import NavBar from './components/Navbar/Navbar.jsx';
 export default function App() {
 
   fetchDataList()
-
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
+  
   return (
 
     <div className='app'>
