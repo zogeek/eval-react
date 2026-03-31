ef7d2c4bf8546a745783b101cb46cd8d3f958adf
zogeek <91289024+zogeek@users.noreply.github.com>
2026-03-31 14:19:33 +0200

    add details view

--- src/pages/list/detail/detail.jsx
+++ src/pages/list/detail/detail.jsx
@@ -1,9 +1,29 @@
 import "./detail.css"
+import Detail from "../../../components/Detail/detail.jsx"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import fetchRecipeData from "../../../services/api/getRecipe.js";
 
-export default function detail() {
+export default function DetailPage() {
+    const { id } = useParams();
+    const [recipe, setRecipe] = useState(null)
+
+    useEffect(() => {
+        const getRecipe = async () => {
+            const recipeData = await fetchRecipeData(id);
+            setRecipe(recipeData)
+        }
+
+        if (id) {
+            getRecipe()
+        }
+    }, [id]);
+
+    console.log(recipe);
+
     return (
         <>
-            Detail
+            <Detail recipe={recipe} />
         </>
     )
 }
