9b1cd71dbb2e3c303d10d142921f609792c698ce
Alexis Hazebrouck <alexis.hazebrouck@enigma-school.com>
2026-03-31 11:14:56 +0200

    feat(routage) init routage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar/Navbar"
+import Home from "./pages/home/home"
 
 function App() {
   return (
     <>
-
+      <div>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/list" element={<Layout />} >
+              <Route path="" element={<List />} />
+              <Route path=":id" element={<Details />} />
+            </Route>
+            <Route path="/favoris" element={<Favoris />} />
+          </Routes>
+        </BrowserRouter>
+        </div>
     </>
   )
 }
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,9 @@
+import "./Home.css"
+
+export default function Home(){
+    return (
+        <>
+            <h1>Home</h1>
+        </>
+    )
+}
